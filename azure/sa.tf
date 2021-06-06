6ea7db7aaff11105258f235ffffab9bd4b37cf4a
dveselka <daniel.veselka@gmail.com>
2021-06-06 22:45:19 +0200

    Terraform Azure add containerwq

--- azure/sa.tf
+++ azure/sa.tf
@@ -5,10 +5,13 @@ resource "azurerm_storage_account" "davestorageaccounttf" {
   location                 = "westeurope"
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  access_tier               = "Hot"
+  allow_blob_public_access  = true
+  enable_https_traffic_only = true
 
   network_rules {
-    default_action             = "Deny"
-    ip_rules                   = ["100.0.0.1"]
+    default_action             = "Allow"
+    ip_rules                   = ["<MY-OWN-IP-ADDRESS"]
     virtual_network_subnet_ids = [azurerm_subnet.daveterraformsubnet.id]
     bypass                     = ["Metrics", "AzureServices"]
   }
@@ -18,4 +21,8 @@ resource "azurerm_storage_account" "davestorageaccounttf" {
   }
 }
 
-
+resource "azurerm_storage_container" "davecontainer" {
+  name                  = "davecontainer"
+  storage_account_name  = azurerm_storage_account.davestorageaccounttf.name
+  container_access_type = "blob"
+}
